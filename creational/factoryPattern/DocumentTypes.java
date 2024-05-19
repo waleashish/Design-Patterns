1ffdb9be40eb825b6a14f883c96789e9ccf3f455
waleashish <ashishwale747@gmail.com>
2024-05-19 12:16:21 -0700

    Add constant file representing file types

---
+++ creational/factoryPattern/DocumentTypes.java
@@ -0,0 +1,12 @@
+package creational.factoryPattern;
+
+
+/**
+ * This class represents the types of documents that can be created.
+ * 
+ * @author Ashish Wale
+ */
+public class DocumentTypes {
+    public static final String PDF = "PDF";
+    public static final String WORD = "Word";
+}
