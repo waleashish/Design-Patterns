6cc6f86b283db116b7c0395d9cb6a8f37b9eb7ea
waleashish <ashishwale747@gmail.com>
2024-05-20 16:35:12 -0700

    Add payment types constant

---
+++ creational/abstractFactoryPattern/PaymentTypeConstants.java
@@ -0,0 +1,13 @@
+package creational.abstractFactoryPattern;
+
+
+/**
+ * This class defines constants for different payment types.
+ * 
+ * @author Ashish Wale
+ */
+public class PaymentTypeConstants {
+    public static final String PAYPAL = "PAYPAL";
+    public static final String UPI = "UPI";
+    public static final String CARD = "CARD";
+}
